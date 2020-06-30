347862f60c6a38dc2fe8e1bc157657ce6d6b185c
KevinF <kevnaf@gmail.com>
2020-06-30 11:43:07 -0700

    sql stored procedures first pass

--- DBScripts/spGetPrizesByTournament.sql
+++ DBScripts/spGetPrizesByTournament.sql
@@ -18,7 +18,7 @@ GO
 -- =============================================
 -- Author:		<Kevin Farrow>
 -- Create date: <6/29/2020>
--- Description:	<>
+-- Description:	<Tournament Tracker get calls>
 -- =============================================
 CREATE PROCEDURE [dbo].[spPrizes_GetByTournament]
 	@TournamentId int
@@ -31,7 +31,81 @@ BEGIN
 
     select p.*
 	from dbo.Prizes p
-	inner join dbo.TournamnetPrizes t on p.id = t.prize_id
+	inner join dbo.TournamentPrizes t on p.prize_id = t.prize_id
 	where t.tournament_id = @TournamentId;
+END
+GO
+
+CREATE PROCEDURE dbo.spMatchupEntries_GetByMatchup
+	@MatchupId int
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    select m.*
+	from dbo.Matchups m
+	inner join dbo.MatchupEntries me on m.matchup_id = me.matchup_id
+	where m.matchup_id = @MatchupId;
+END
+GO
+
+CREATE PROCEDURE dbo.spTeam_GetByTournament
+	@TournamentId int
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    select t.*
+	from dbo.Teams t
+	inner join dbo.TournamentEntries te on t.team_id = te.team_id
+	where te.tournament_id = @TournamentId;
+END
+GO
+
+CREATE PROCEDURE dbo.spPeople_GetAll
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    select *
+	from dbo.People;
+END
+GO
+
+CREATE PROCEDURE dbo.spTeamMembers_GetByTeamId
+	@TeamId int
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    select tm.*
+	from dbo.TeamMembers tm
+	inner join dbo.Teams t on tm.team_id = t.team_id
+	where tm.team_id = @TeamId;
+END
+GO
+
+CREATE PROCEDURE dbo.spTournaments_GetAll
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    select *
+	from dbo.Tournaments;
 END
 GO
